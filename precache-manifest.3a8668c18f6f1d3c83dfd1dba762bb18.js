7eb708cb94fe60521739236b041bf95c60b14861
Marcinho <marcin1897@gmail.com>
2020-05-04 20:48:28 +0200

    Updates

---
+++ precache-manifest.3a8668c18f6f1d3c83dfd1dba762bb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764c7f07b73d61e655fb095b20e61a9e",
+    "url": "/react_ivation/index.html"
+  },
+  {
+    "revision": "6dc8a90149b979235d73",
+    "url": "/react_ivation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c067576510c53eff2909",
+    "url": "/react_ivation/static/js/2.0857f0d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_ivation/static/js/2.0857f0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc8a90149b979235d73",
+    "url": "/react_ivation/static/js/main.a3de8ffd.chunk.js"
+  },
+  {
+    "revision": "d6f9dc19bf8b5f29ec2a",
+    "url": "/react_ivation/static/js/runtime-main.e597a3ff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Marcinho92.github.io/precache-manifest.9edfb6d08e78b375ab697892ba7d75ce.js"
+  "/react_ivation/precache-manifest.3a8668c18f6f1d3c83dfd1dba762bb18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Marcinho92.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_ivation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
