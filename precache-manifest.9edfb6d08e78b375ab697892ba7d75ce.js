df36a3377d785895fc64dbc9dcf7de6138e61607
Marcinho <marcin1897@gmail.com>
2020-05-04 20:38:44 +0200

    Updates

---
+++ precache-manifest.9edfb6d08e78b375ab697892ba7d75ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68f790c425395a70ba976cbb157d5dd",
+    "url": "/Marcinho92.github.io/index.html"
+  },
+  {
+    "revision": "9b9019c5ca6e098521c9",
+    "url": "/Marcinho92.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c067576510c53eff2909",
+    "url": "/Marcinho92.github.io/static/js/2.0857f0d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Marcinho92.github.io/static/js/2.0857f0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9019c5ca6e098521c9",
+    "url": "/Marcinho92.github.io/static/js/main.95676a8d.chunk.js"
+  },
+  {
+    "revision": "aa18e9778abdd8dd6dc5",
+    "url": "/Marcinho92.github.io/static/js/runtime-main.bb767a68.js"
+  }
+]);
